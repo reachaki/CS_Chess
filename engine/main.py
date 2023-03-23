ebc49a0d3a313c559657a2b3b5b9cfbdb79e76cb
Akirioz <akshyatdali@gmail.com>
2023-03-23 19:02:18 +0000

    click release

--- engine/main.py
+++ engine/main.py
@@ -4,6 +4,8 @@ import pygame
 import sys
 from const import *
 from game import Game
+from square import Square
+from move import Move
 
 
 class Main:
@@ -70,6 +72,26 @@ class Main:
 
                 # release
                 elif event.type == pygame.MOUSEBUTTONUP:
+
+                    if dragger.dragging:
+                        dragger.update_mouse(event.pos)
+
+                        release_row = dragger.mouseY // SQSIZE
+                        release_col = dragger.mouseX // SQSIZE
+
+                        # create possible move
+                        initial = Square(dragger.initial_row,
+                                         dragger.initial_col)
+                        final = Square(release_row, release_col)
+                        move = Move(initial, final)
+
+                        # valid move?
+                        if board.valid_move(dragger.piece, move):
+                            board.move(dragger.piece, move)
+                            # show methods
+                            game.show_bg(screen)
+                            game.show_pieces(screen)
+
                     dragger.undrag_piece()
 
                 # quit
