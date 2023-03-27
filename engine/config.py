2a29498d8071befa411e1708e7ab244c055aaaf4
Akirioz <akshyatdali@gmail.com>
2023-03-27 22:26:05 +0100

    sound

--- engine/config.py
+++ engine/config.py
@@ -1,11 +1,25 @@
+import pygame
+import os
+from sound import Sound
+from theme import Theme
+
 
 class Config:
 
     def __init__(self):
-        pass
+        self.themes = []
+        self._add_themes()
+        self.idx = 0
+        self.theme = self.themes[self.idx]
+        # font
+        self.move_sound = Sound(
+            os.path.join('assets/sounds/move.wav'))
+        self.capture_sound = Sound(
+            os.path.join('assets/sounds/capture.wav'))
 
     def change_theme(self):
-        pass
+        self.idx += 1
+        self.idx %= len(self.themes)
 
     def _add_themes(self):
         pass
